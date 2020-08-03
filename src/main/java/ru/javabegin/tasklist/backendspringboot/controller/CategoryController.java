dcc694c013e1bb148cd21d3bd5429dadb0d33795
Illya Sokolov <illya.sokolov82@gmail.com>
2020-08-03 16:14:03 +0300

    ResponseEntity, PutMapping, PostMpping

--- src/main/java/ru/javabegin/tasklist/backendspringboot/controller/CategoryController.java
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package ru.javabegin.tasklist.backendspringboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.javabegin.tasklist.backendspringboot.entity.Category;
 import ru.javabegin.tasklist.backendspringboot.repo.CategoryRepository;
@@ -9,7 +11,7 @@ import java.util.List;
 // используем @RestController вместо обычного @Controller, чтобы все ответы сразу оборачивались в JSON
 // иначе пришлось бы выполнять лишнюю работу, использовать @ResponseBody для ответа, указывать тип отправки JSON
 @RestController
-@RequestMapping("/category")
+@RequestMapping ("/category") // базовый адрес
 public class CategoryController {
 
     // доступ к данным из БД
@@ -24,14 +26,37 @@ public class CategoryController {
     // для тестирования адрес: http://localhost:8080/category/test
     @GetMapping("/test")
     public List<Category> test() {
-        List<Category> categories = categoryRepository.findAll();
+        List<Category> list = categoryRepository.findAll();
 
-        return categories;
+        return list; // JSON формат будет использоваться автоматически
     }
 
     @PostMapping("/add")
-    public void add(@RequestBody Category category) {
-        categoryRepository.save(category);
+    public ResponseEntity<Category> add(@RequestBody Category category){
+        // проверка на обязательные параметры
+        if (category.getId() != null && category.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение title
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Category category){
+        // проверка на обязательные параметры
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение title
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // save работает как на добавление, так и на обновление
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 
 }
--- src/main/java/ru/javabegin/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,5 +1,7 @@
 package ru.javabegin.tasklist.backendspringboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.javabegin.tasklist.backendspringboot.entity.Priority;
 import ru.javabegin.tasklist.backendspringboot.repo.PriorityRepository;
@@ -26,13 +28,48 @@ public class PriorityController {
     @GetMapping("/test")
     public List<Priority> test() {
         List<Priority> list = priorityRepository.findAll();
+
         return list; // JSON формат будет использоваться автоматически
     }
 
+
     @PostMapping("/add")
-    public void add(@RequestBody Priority priority) {
-        priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority){
+        // проверка на обязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // save работает как на добавление, так и на обновление
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Priority priority){
+        // проверка на обязательные параметры
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // save работает как на добавление, так и на обновление
+        return ResponseEntity.ok(priorityRepository.save(priority));
+
+    }
 
 }
